1391ebd31e2937929d62c25c03952e6bf34fa629
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2018-11-04 23:15:00 +0530

    Added a content script that load at the end of page load

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,36 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const path = require('path');
 
 module.exports = {
-    entry: {
-        'init': './src/init.js',
-        'options': './src/options/options.js',
-        'popup': './src/popup/popup.js',
-    },
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: '[name].js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.vue$/,
-                loader: 'vue-loader'
-            },
-            {
-                test: /\.js$/,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'vue-style-loader',
-                    'css-loader'
-                ]
-            }
+  entry: {
+    'init': './src/init.js',
+    'options': './src/options/options.js',
+    'popup': './src/popup/popup.js',
+    'app': './src/app/app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
         ]
-    },
-    plugins: [
-        new VueLoaderPlugin()
+      }
     ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
 };
